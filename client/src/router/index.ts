324b76806e9755d393678737e80122163281a61e
Ernis <ernest.jsdev@gmail.com>
2025-10-24 21:15:46 +0600

    feat: Add routes for Workbench and Trading pages

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import Workbench from '@/views/Workbench.vue'
+import Trading from '@/views/Trading.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/workbench',
+    name: 'Workbench',
+    component: Workbench
+  },
+  {
+    path: '/trading',
+    name: 'Trading',
+    component: Trading
   }
 ]
 
